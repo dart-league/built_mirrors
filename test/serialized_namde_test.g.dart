13ff1d1714e88d0646659021eac3213f238ebc36
Luis Vargas <luisbigvargas@gmail.com>
2018-08-03 19:52:27 -0400

    0.10.0 - add `SerializedName` annotation

---
+++ test/serialized_namde_test.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of built_mirrors.serialized_name;
+
+// **************************************************************************
+// MirrorsGenerator
+// **************************************************************************
+
+_ClassWithSerializedName__Constructor([positionalParams, namedParams]) =>
+    new ClassWithSerializedName();
+
+const $$ClassWithSerializedName_fields_renamed = const DeclarationMirror(
+  name: 'the_renamed',
+  type: dynamic,
+);
+
+const ClassWithSerializedNameClassMirror = const ClassMirror(
+    name: 'ClassWithSerializedName',
+    constructors: const {
+      '': const FunctionMirror(
+          name: '', $call: _ClassWithSerializedName__Constructor)
+    },
+    fields: const {
+      'the_renamed': $$ClassWithSerializedName_fields_renamed
+    },
+    getters: const [
+      'the_renamed'
+    ],
+    setters: const [
+      'the_renamed'
+    ]);
+
+// **************************************************************************
+// InitMirrorsGenerator
+// **************************************************************************
+
+_initMirrors() {
+  initClassMirrors(
+      {ClassWithSerializedName: ClassWithSerializedNameClassMirror});
+}
